dc96dd6afd836027ae0ac764dce336d7fb9891dd
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2023-09-23 14:10:57 +0200

    feature: aboutMe component

--- src/app/components/aboutme/aboutme.jsx
+++ src/app/components/aboutme/aboutme.jsx
@@ -2,9 +2,45 @@ import "./aboutme.css";
 export default function AboutMe() {
   return (
     <div className="AboutMeWrapper" id="AboutMe">
-      <h1 contenteditable spellcheck="false">
-        Szymon Marcinkowski
-      </h1>
+      <h1>Szymon Marcinkowski</h1>
+      <h2 class="SectionTitle">AboutMe()</h2>
+      <div className="AbouMeContent">
+        <div className="SquareLeftoToRight"></div>
+        <div className="SquareLeftoToBottom"></div>
+        <div className="SquareRightToTop"></div>
+        <div className="SquareRightToLeft"></div>
+        <div className="AboutMePhoto0">
+          <img src="./img/me.png"></img>
+        </div>
+        <div className="AboutMePhoto1">
+          <img src="./img/me.png"></img>
+        </div>
+        <div className="AboutMePhoto2">
+          <img src="./img/me.png"></img>
+        </div>
+        <div class="AboutMeInterests">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum
+          condimentum posuere. Suspendisse egestas, turpis sit amet congue
+          pulvinar, ligula odio malesuada est, a consequat lacus augue sed sem.
+          Suspendisse consequat aliquet volutpat. Proin erat tellus, bibendum
+          pretium libero at, cursus laoreet purus. Duis fermentum justo sit amet
+          urna fermentum consequat. Aliquam lacinia lectus nec vehicula posuere.
+          Nam ultrices lectus odio, at consequat quam dictum eget. Nunc
+          efficitur sed justo ut feugiat. Fusce sagittis leo et arcu laoreet
+          suscipit.
+        </div>
+        <div class="AboutMeInterests">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum
+          condimentum posuere. Suspendisse egestas, turpis sit amet congue
+          pulvinar, ligula odio malesuada est, a consequat lacus augue sed sem.
+          Suspendisse consequat aliquet volutpat. Proin erat tellus, bibendum
+          pretium libero at, cursus laoreet purus. Duis fermentum justo sit amet
+          urna fermentum consequat. Aliquam lacinia lectus nec vehicula posuere.
+          Nam ultrices lectus odio, at consequat quam dictum eget. Nunc
+          efficitur sed justo ut feugiat. Fusce sagittis leo et arcu laoreet
+          suscipit.
+        </div>
+      </div>
     </div>
   );
 }
